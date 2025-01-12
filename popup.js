3d5ee461b677831ea804194984022e7d50b0e669
ericlin-98 <ericlinyunkai@gmail.com>
2025-01-12 00:54:14 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
         type="text" 
         id="goalInput" 
         class="owl-input" 
-        placeholder="e.g. finishing Calc 1 homework, edit a video..."
+        placeholder="e.g. finishing calc 1 homework, edit a video..."
       />
       <button id="startBtn" class="owl-button">Start Tracking</button>
       <p id="statusMessage" class="owl-status"></p>
